1eb01ea65e2916c7fe97b0356c9e812e6267de96
ArIonMark1 <andreydatsenko777@gmail.com>
2021-02-24 11:29:47 +0100

    html

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {'title': 'GeekShop - Products'}
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {'title': 'GeekShop - Contacts'}
+    return render(request, 'mainapp/contact.html', context)
